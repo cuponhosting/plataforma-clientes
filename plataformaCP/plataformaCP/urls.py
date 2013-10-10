5445dda84c6a8380c7c827a8f6d33529f63c4f49
Julian Bohorquez <julianarmando@gmail.com>
2013-10-10 03:50:17 -0500

    Agrego bases de datos, configuracion y modelos

--- plataformaCP/plataformaCP/urls.py
+++ plataformaCP/plataformaCP/urls.py
@@ -18,4 +18,3 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
-
--- plataformaCP/principal/models.py
+++ plataformaCP/principal/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class dominios(models.Model):
+	codigo = models.DecimalField(max_digits=10, decimal_places=0)
+	dominio = models.CharField(max_length=30)
+	fecha_compra = models.DateField()
+	duracion = models.CharField(max_length=30)
+	fecha_vencimiento = models.DateField()
+
+
+
+class hosting(models.Model):
+	codigo = models.DecimalField(max_digits=10, decimal_places=0)
+	plan = models.CharField(max_length=30)
+	dominio_principal = models.CharField(max_length=30)
+	fecha_compra = models.DateField()
+	duracion = models.CharField(max_length=30)
+	fecha_vencimiento = models.DateField()
