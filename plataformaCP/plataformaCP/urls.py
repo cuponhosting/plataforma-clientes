d06ebf5e5c10153762371dde79d653dbf09ba6d1
Julian Bohorquez <julianarmando@gmail.com>
2013-10-10 03:07:17 -0500

    Agrego una plantilla de muestra y la estructura de urls de la primera version de la plataforma

--- plataformaCP/plataformaCP/urls.py
+++ plataformaCP/plataformaCP/urls.py
@@ -7,6 +7,9 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'principal.views.home', name='home'),
+    url(r'^login/$', 'principal.views.login', name='login'),
+    url(r'^servicios/$', 'principal.views.servicios', name='servicios'),
+    url(r'^cerrar-sesion/$', 'principal.views.logout', name='logout'),
     # url(r'^plataformaCP/', include('plataformaCP.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -15,3 +18,4 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
--- plataformaCP/principal/views.py
+++ plataformaCP/principal/views.py
@@ -1,5 +1,16 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def home(request):
-	return HttpResponse("Hola")
+	return HttpResponse("Este redirecciona a servicios o a login si no ha iniciado sesion")
+
+def login(request):
+	return HttpResponse("Aqui va el formulario de login")	
+
+def servicios(request):
+	tal = "tallllll"
+	return render_to_response("servicios.html", {'tal':tal})	
+
+def logout(request):
+	return HttpResponse("Se hace el cierre de la sesion")		
