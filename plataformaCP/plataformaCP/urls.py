056a3cc5c1491ec45bd32f448cd820e6a1340f64
Julian Bohorquez <julianarmando@gmail.com>
2013-10-10 02:35:18 -0500

    Agrego la primer url y vista a modo de ejemplo y basico para tener idea de la estructura del proyecto

--- plataformaCP/plataformaCP/urls.py
+++ plataformaCP/plataformaCP/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'plataformaCP.views.home', name='home'),
+    url(r'^$', 'principal.views.home', name='home'),
     # url(r'^plataformaCP/', include('plataformaCP.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
