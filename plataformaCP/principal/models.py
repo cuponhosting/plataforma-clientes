6e1bbecd44b4c232e3713297966c4904520a8cd1
Julian Bohorquez <julianarmando@gmail.com>
2013-10-11 02:27:19 -0500

    Hice cambios en el admin desde models, agregue y relacione tablas y elimine la base de datos para regenerarla

--- plataformaCP/principal/models.py
+++ plataformaCP/principal/models.py
@@ -1,20 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class dominios(models.Model):
 	codigo = models.DecimalField(max_digits=10, decimal_places=0)
+	usuario = models.ForeignKey(User)
 	dominio = models.CharField(max_length=30)
 	fecha_compra = models.DateField()
 	duracion = models.CharField(max_length=30)
 	fecha_vencimiento = models.DateField()
-
-
+	
+	def __unicode__(self):
+		return "%s" % (self.dominio)
 
 
 class hosting(models.Model):
 	codigo = models.DecimalField(max_digits=10, decimal_places=0)
+	usuario = models.ForeignKey(User)
 	plan = models.CharField(max_length=30)
 	dominio_principal = models.CharField(max_length=30)
 	fecha_compra = models.DateField()
 	duracion = models.CharField(max_length=30)
 	fecha_vencimiento = models.DateField()
+
+	def __unicode__(self):
+		return "%s - %s" % (self.dominio_principal, self.usuario)
