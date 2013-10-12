9bf1e9edbb86cb9de6ea94ad92cdfc5e7dd521c3
Julian Bohorquez <julianarmando@gmail.com>
2013-10-12 02:50:49 -0500

    Aun no funciona el envio de datos a el template, agregue algunos ejemplos

--- plataformaCP/principal/views.py
+++ plataformaCP/principal/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from principal.models import *
 
 #def home(request):
 #	return HttpResponse("Este redirecciona a servicios o a login si no ha iniciado sesion")
@@ -13,7 +14,12 @@ def login(request):
 @login_required(login_url='/login')
 def servicios(request):
 	tal = "tallll333333ll"
+	
 	#tal = request.user.username
+	
+	#tal =  dominios.objects.all().filter(dominio='juarbo.com')
+	#tal =  dominios.objects.get(codigo='2342')
+	#tal =  dominios.objects.get
 	return render_to_response("servicios.html", {'tal':tal})	
 
 @login_required(login_url='/login')
